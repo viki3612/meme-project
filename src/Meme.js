9f540454be2cf63c3c119cbc3b8c67b3d18f6df2
viki3612 <viktorianikolova95@gmail.com>
2023-02-17 16:34:13 +0000

    Update Meme.js

--- src/Meme.js
+++ src/Meme.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Meme.css";
 
 export default function Meme() {
+  //let [memeImage, setMemeImage] = useState();
+  // function handleSubmit() {
+  //api call
+  //}
   return (
-    <form className="form">
+    <div className="form">
       <div className="input-group">
         <input type="text" className="form-control" />{" "}
         <input type="text" className="form-control" />
       </div>
-      <button type="submit" className="btn btn-primary btn-lg">
+      <button
+        type="submit"
+        className="btn btn-primary btn-lg"
+        onSubmit={handleSubmit}
+      >
         Get a new meme image
       </button>
-    </form>
+      //
+      <img src={memeImage} className="meme-image" />
+    </div>
   );
 }
